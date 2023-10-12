bb27d5bdfaf226e8881d75bec05b73a76f3dae00
samrat malla <samratmalla48@gmail.com>
2023-10-12 21:31:28 +1100

    Links

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -7,18 +7,34 @@ const Footer = () => {
       <Container fluid className="footer text-white">
         <Container>
           <Row>
-            <Col xs={6}>
+            <Col xs={4}>
+              <h5>Column 1</h5>
               <ul className="list-unstyled">
                 <NavLink href="#">FAQ</NavLink>
                 <NavLink href="#">About Us</NavLink>
                 <NavLink href="#">Terms of Services</NavLink>
               </ul>
             </Col>
-            <Col xs={6}>
+            <Col xs={4}>
+              <h5>Column 2</h5>
               <ul className="list-unstyled">
                 <NavLink href="#">Privacy Policy</NavLink>
                 <NavLink href="#">Careers</NavLink>
-                <NavLink href="#">Shipping and Delivery</NavLink>
+                <NavLink href="#">Shipping and Dev</NavLink>
+              </ul>
+            </Col>
+            <Col xs={4}>
+              <h5>Column 3</h5>
+              <ul className="list-unstyled">
+                <NavLink href="https://www.facebook.com/itsnewcastleautospareparts/">
+                  Facebook
+                </NavLink>
+                <NavLink href="https://www.instagram.com/itsnewcastleautospareparts/">
+                  Instagram
+                </NavLink>
+                <NavLink href="https://www.twitter.com/newcastleasp/">
+                  Twitter
+                </NavLink>
               </ul>
             </Col>
           </Row>
