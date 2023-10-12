05b133fb0b09dee1ee0cf7f1f1a89e3e3466b1dc
samrat malla <samratmalla48@gmail.com>
2023-10-12 21:13:58 +1100

    Footer Update

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,38 +3,28 @@ import { Container, Row, Col, NavLink } from "react-bootstrap";
 const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className='text-white'>
-      <Container fluid className='footer text-white'>
+    <footer className="text-white">
+      <Container fluid className="footer text-white">
         <Container>
           <Row>
-            <Col xs={4}>
-              <h5>Column 1</h5>
-              <ul className='list-unstyled'>
-                <NavLink href='#'>Link 1</NavLink>
-                <NavLink href='#'>Link 2</NavLink>
-                <NavLink href='#'>Link 3</NavLink>
+            <Col xs={6}>
+              <ul className="list-unstyled">
+                <NavLink href="#">FAQ</NavLink>
+                <NavLink href="#">About Us</NavLink>
+                <NavLink href="#">Terms of Services</NavLink>
               </ul>
             </Col>
-            <Col xs={4}>
-              <h5>Column 2</h5>
-              <ul className='list-unstyled'>
-                <NavLink href='#'>Link 1</NavLink>
-                <NavLink href='#'>Link 2</NavLink>
-                <NavLink href='#'>Link 3</NavLink>
-              </ul>
-            </Col>
-            <Col xs={4}>
-              <h5>Column 3</h5>
-              <ul className='list-unstyled'>
-                <NavLink href='#'>Link 1</NavLink>
-                <NavLink href='#'>Link 2</NavLink>
-                <NavLink href='#'>Link 3</NavLink>
+            <Col xs={6}>
+              <ul className="list-unstyled">
+                <NavLink href="#">Privacy Policy</NavLink>
+                <NavLink href="#">Careers</NavLink>
+                <NavLink href="#">Shipping and Delivery</NavLink>
               </ul>
             </Col>
           </Row>
         </Container>
       </Container>
-      <div className='text-center footer-bottom py-3'>
+      <div className="text-center footer-bottom py-3">
         Newcastle Auto Spare Parts Pty Ltd &copy; {currentYear}
       </div>
     </footer>
